a22467865477bfd3b4d30b311bbe7c0a7fda7f3e
Augusto Etz <augustoetz@hotmail.com>
2022-09-08 20:29:27 -0300

    resolvendo a 5

--- src/Pages/Search.js
+++ src/Pages/Search.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Header from '../Components/Header';
-import { getCategories } from '../services/api';
+import { getCategories, getProductsFromCategoryAndQuery } from '../services/api';
 
 class Search extends React.Component {
   constructor(props) {
@@ -8,6 +8,9 @@ class Search extends React.Component {
     this.state = {
       pesquisado: false,
       categorias: [],
+      inputValue: '',
+      radioValue: '',
+      produtos: [],
     };
   }
 
@@ -17,12 +20,30 @@ class Search extends React.Component {
 
   pegandoCategorias = async () => {
     const categorias = await getCategories();
-    console.log(categorias);
     this.setState({ categorias });
   };
 
+  handleChange = (event) => {
+    const { target } = event;
+    this.setState({ inputValue: target.value });
+  };
+
+  handleClickBuscar = async () => {
+    const { inputValue, radioValue } = this.state;
+    let pesq = false;
+    const produtos = await getProductsFromCategoryAndQuery(radioValue, inputValue);
+    console.log(produtos)
+    if (produtos.results.length === 0) { pesq = true; }
+    this.setState({ produtos: produtos.results, pesquisado: pesq });
+  };
+
+  radioClick = (event) => {
+    const { target } = event;
+    if (target.checked) { this.setState({ radioValue: target.value }); }
+  };
+
   render() {
-    const { pesquisado, categorias } = this.state;
+    const { categorias, inputValue, produtos, pesquisado } = this.state;
     return (
       <div>
         <Header />
@@ -36,18 +57,39 @@ class Search extends React.Component {
                 name="categoria"
                 id={ categoria.id }
                 type="radio"
+                onClick={ this.radioClick }
               />
             </label>
           ))}
-          <input type="text" />
+          <input
+            type="text"
+            data-testid="query-input"
+            onChange={ this.handleChange }
+            value={ inputValue }
+          />
+          <button
+            type="button"
+            data-testid="query-button"
+            onClick={ this.handleClickBuscar }
+          >
+            Buscar
+
+          </button>
         </section>
         <section className="Produtos">
-          {pesquisado === false ? (
-            <p data-testid="home-initial-message">
-              Digite algum termo de pesquisa ou escolha uma categoria.
-
-            </p>)
-            : (null)}
+          <p data-testid="home-initial-message">
+            Digite algum termo de pesquisa ou escolha uma categoria.
+          </p>
+          <section>
+            {produtos.map((produto) => (
+              <section data-testid="product" key={ produto.id }>
+                <h4>{produtos.title}</h4>
+                <h5>{`Preço: R$ ${produto.price}`}</h5>
+                <img src={ produto.thumbnail } alt={ produto.id } />
+              </section>
+            ))}
+            {pesquisado === true ? (<p>Nenhum produto foi encontrado</p>) : (null)}
+          </section>
         </section>
       </div>
     );
